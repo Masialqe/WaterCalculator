c435306cfe1ba8917525ce83b4142ff4e40e7b1a
Masa <j.zasadzki@interia.pl>
2026-05-29 17:31:29 +0200

    Add first handler

---
+++ WaterCalculator/Features/Apartments/Create/CreateApartmentHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WaterCalculator.Database;
+
+namespace WaterCalculator.Features.Apartments.Create
+{
+    public record CreateApartmentCommand(string Name, string Details, Guid? GroupId);
+    public class CreateApartmentHandler(IDbContextFactory<DatabaseContext> dbContextFactory, 
+        ILogger<CreateApartmentHandler> logger)
+    {
+        public async Task HandleAsync(CreateApartmentCommand command, CancellationToken cancellationToken = default)
+        {
+
+        }
+    }
+}
